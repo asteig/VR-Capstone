a1dcd6d84b0c2d3b0307e74cba8f6c3e1eefaf15
Amanda Steig <asteig@gmail.com>
2018-05-28 14:08:58 -0700

    Added dungeon maze

---
+++ Assets/Scripts/GetSize.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log (GameObject.tag);
+        Collider m_collider = GetComponent<Collider>();
+		Debug.Log (m_collider.bounds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
