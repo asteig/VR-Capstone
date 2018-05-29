daa3864911594f00bda1d55c3191498996d27e0e
Amanda Steig <asteig@gmail.com>
2018-05-28 17:32:27 -0700

    Added locomotion

---
+++ Assets/Scripts/ToggleWalk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWalk : MonoBehaviour
+{
+
+    public Transform vrCamera;
+    public float speed = 1.0f;
+    public bool moveForward = false;
+
+    private Rigidbody cc;
+
+    // Use this for initialization
+    void Start()
+    {
+        cc = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            moveForward = !moveForward;
+        }
+
+        if (moveForward)
+        {
+            cc.velocity = vrCamera.transform.forward * speed;
+        }
+        else
+        {
+            cc.velocity = Vector3.zero;
+        }
+
+    }
+
+}
