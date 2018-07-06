b28605f00cc6b04c8d9f369c7c248003f3b484bd
Amanda Steig <asteig@gmail.com>
2018-07-06 11:21:03 -0700

    Added test networking scene

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+		if(isLocalPlayer) {
+            GetComponent<ToggleWalk>().enabled = true;    
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
